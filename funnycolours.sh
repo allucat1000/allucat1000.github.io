a7460080adbbfcea31b080e962061ddb303f910b
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-10-30 09:37:12 +0200

    funnycolours.sh

---
+++ funnycolours.sh
@@ -0,0 +1,105 @@
+while true; do
+    color black
+    echo h
+    sleep 0.015
+
+    color magenta
+    echo hello
+    sleep 0.015
+
+    color blue
+    echo hellohell
+    sleep 0.015
+
+    color cyan
+    echo hellohellohel
+    sleep 0.015
+
+    color green
+    echo hellohellohellohe
+    sleep 0.015
+
+    color yellow
+    echo hellohellohellohelloh
+    sleep 0.015
+
+    color red
+    echo hellohellohellohellohello
+    sleep 0.015
+
+    color white
+    echo hellohellohellohellohellohell
+    sleep 0.015
+
+    color green
+    echo hellohellohellohellohellohellohel
+    sleep 0.015
+
+    color yellow
+    echo hellohellohellohellohellohellohellohe
+    sleep 0.015
+
+    color red
+    echo hellohellohellohellohellohellohellohelloh
+    sleep 0.015
+
+    color blue
+    echo hellohellohellohellohellohellohellohellohello
+    sleep 0.015
+
+    color cyan
+    echo hellohellohellohellohellohellohellohellohellohell
+    sleep 0.015
+
+    color yellow
+    echo hellohellohellohellohellohellohellohellohellohello
+    sleep 0.015
+
+    color green
+    echo hellohellohellohellohellohellohellohellohellohe
+    sleep 0.015
+
+    color cyan
+    echo hellohellohellohellohellohellohellohellohel
+    sleep 0.015
+
+    color blue
+    echo hellohellohellohellohellohellohellohell
+    sleep 0.015
+
+    color magenta
+    echo hellohellohellohellohellohellohello
+    sleep 0.015
+
+    color black
+    echo hellohellohellohellohellohelloh
+    sleep 0.015
+
+    color red
+    echo hellohellohellohellohellohe
+    sleep 0.015
+
+    color green
+    echo hellohellohellohellohel
+    sleep 0.015
+
+    color blue
+    echo hellohellohellohell
+    sleep 0.015
+
+    color yellow
+    echo hellohellohello
+    sleep 0.015
+
+    color red
+    echo hellohelloh
+    sleep 0.015
+
+    color cyan
+    echo hellohe
+    sleep 0.015
+
+    color magenta
+    echo hel
+    sleep 0.015
+done
