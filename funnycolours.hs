a85c87a2acff77c6229b699c8b702c6cdf4ee8fe
allucat1000 <79474282+allucat1000@users.noreply.github.com>
2025-10-30 09:40:11 +0200

    funnycolours.hs

--- funnycolours.hs
+++ funnycolours.hs
@@ -1,105 +1,105 @@
 while true; do
-    color black
+    color fg black
     echo h
     sleep 0.015
 
-    color magenta
+    color fg magenta
     echo hello
     sleep 0.015
 
-    color blue
+    color fg blue
     echo hellohell
     sleep 0.015
 
-    color cyan
+    color fg cyan
     echo hellohellohel
     sleep 0.015
 
-    color green
+    color fg green
     echo hellohellohellohe
     sleep 0.015
 
-    color yellow
+    color fg yellow
     echo hellohellohellohelloh
     sleep 0.015
 
-    color red
+    color fg red
     echo hellohellohellohellohello
     sleep 0.015
 
-    color white
+    color fg white
     echo hellohellohellohellohellohell
     sleep 0.015
 
-    color green
+    color fg green
     echo hellohellohellohellohellohellohel
     sleep 0.015
 
-    color yellow
+    color fg yellow
     echo hellohellohellohellohellohellohellohe
     sleep 0.015
 
-    color red
+    color fg red
     echo hellohellohellohellohellohellohellohelloh
     sleep 0.015
 
-    color blue
+    color fg blue
     echo hellohellohellohellohellohellohellohellohello
     sleep 0.015
 
-    color cyan
+    color fg cyan
     echo hellohellohellohellohellohellohellohellohellohell
     sleep 0.015
 
-    color yellow
+    color fg yellow
     echo hellohellohellohellohellohellohellohellohellohello
     sleep 0.015
 
-    color green
+    color fg green
     echo hellohellohellohellohellohellohellohellohellohe
     sleep 0.015
 
-    color cyan
+    color fg cyan
     echo hellohellohellohellohellohellohellohellohel
     sleep 0.015
 
-    color blue
+    color fg blue
     echo hellohellohellohellohellohellohellohell
     sleep 0.015
 
-    color magenta
+    color fg magenta
     echo hellohellohellohellohellohellohello
     sleep 0.015
 
-    color black
+    color fg black
     echo hellohellohellohellohellohelloh
     sleep 0.015
 
-    color red
+    color fg red
     echo hellohellohellohellohellohe
     sleep 0.015
 
-    color green
+    color fg green
     echo hellohellohellohellohel
     sleep 0.015
 
-    color blue
+    color fg blue
     echo hellohellohellohell
     sleep 0.015
 
-    color yellow
+    color fg yellow
     echo hellohellohello
     sleep 0.015
 
-    color red
+    color fg red
     echo hellohelloh
     sleep 0.015
 
-    color cyan
+    color fg cyan
     echo hellohe
     sleep 0.015
 
-    color magenta
+    color fg magenta
     echo hel
     sleep 0.015
 done
